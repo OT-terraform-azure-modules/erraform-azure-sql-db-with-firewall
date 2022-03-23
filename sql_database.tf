98ec2b398df972bf81bde865a54e1212ddaccd1f
Rishabh Sharma <93264358+rishabhhsharmaa@users.noreply.github.com>
2022-03-23 12:41:20 +0530

    Update sql_database.tf

--- sql_database.tf
+++ sql_database.tf
@@ -1,6 +1,6 @@
 resource "azurerm_sql_database" "main" {
   count                            = var.sql_database_required == true ? length(var.azurerm_sql_database_name) : 0
-  name                             = "${var.azurerm_sql_database_name}-${count.index}"
+  name                             = var.azurerm_sql_database_name[count.index]
   resource_group_name              = var.resource_group_name
   location                         = element(var.regions, count.index)
   server_name                      = azurerm_sql_server.main[0].name
